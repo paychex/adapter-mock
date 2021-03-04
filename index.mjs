18280bb717498e38e9c23ee274c88c2fe6ad7dc0
Barnes, Dan <dbarnes1@paychex.com>
2021-03-04 13:33:27 -0500

    docs: improve intellisense

--- index.mjs
+++ index.mjs
@@ -104,18 +104,7 @@ import {
     defaultsDeep,
 } from 'lodash-es';
 
-import { data as Types } from '@paychex/core/dist/esm/types.mjs';
-
-class Request extends Types.Request {}
-class Response extends Types.Response {}
-class ProxyRule extends Types.ProxyRule {}
-
-/**
- * @async
- * @callback Adapter
- * @param {Request}
- * @returns {Promise.<Response>}
- */
+import '@paychex/core/types/data.mjs';
 
 const DEFAULT_RESPONSE = Object.freeze({
     meta: {
@@ -144,10 +133,35 @@ function asConditionPair([condition, factory]) {
 }
 
 /**
+ * @global
+ * @class
+ * @hideconstructor
+ * @extends Array
+ */
+class MockRule extends Array {
+
+    /**
+     * Lodash iteratee to use; if it returns a truthy value, the associated
+     * Response will be returned.
+     *
+     * @type {function|object|string[]}
+     */
+    [0] = null
+
+    /**
+     * The Response to return if the associated iteratee returns a truthy value.
+     *
+     * @type {Response}
+     */
+    [1] = null
+
+}
+
+/**
  * A data adapter that returns mock Responses based on conditions matching Requests.
  *
  * @function
- * @param {ProxyRule[]} rules The rules specifying which mock Responses should be returned
+ * @param {MockRule[]} rules The rules specifying which mock Responses should be returned
  * based on which conditions match incoming Requests.
  * @returns {Adapter} The adapter to register with a data pipeline.
  * @example
